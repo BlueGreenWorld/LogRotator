f29bacbd63ed951ea8707cf0f2006acbcd2652df
Faraz Masood Khan <faraz@fanaticlab.com>
2013-03-12 23:57:00 +0500

    Removed docs

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({objects:{},terms:{index:[0,1],search:[0,1],welcom:[0,1],modul:[0,1],indic:[0,1],content:[0,1],tabl:[0,1],document:[0,1],page:[0,1],logrot:[0,1]},objtypes:{},titles:["Welcome to LogRotator&#8217;s documentation!","Welcome to LogRotator&#8217;s documentation!"],objnames:{},filenames:["test","index"]})
